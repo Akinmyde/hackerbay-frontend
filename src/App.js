0c528d2938e66f9547ac0a062ed634cb41b05017
Akinremi Olumide <akinremiolumide@Akinremis-MacBook-Pro.local>
2019-03-04 21:31:14 +0100

    [Feature] Implement Box Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+/* eslint-disable no-unused-expressions */
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from '../src/components/form'
+import Box from '../src/components/box'
+import validate from 'validate.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 10, height: 10, clicked: true, error: '' }
+  }
   render() {
+    const onInputChange = e => {
+      const { value, name } = e.target;
+      const userInput = parseInt(value, 10)
+      if (userInput > 0 && validate.isNumber(userInput)) {
+        if (name === 'height') {
+          return this.setState({height: userInput, error: ''})
+        } return this.setState({width: userInput, error: ''})
+      } return this.setState({error: 'Please enter a valid input (hint: must be a number)'})
+    };
+
+    const onButtonClick = e => {
+      e.preventDefault();
+      const height = document.getElementById('height').value;
+      const width = document.getElementById('width').value;
+
+      if ((!validate.isNumber(parseInt(height, 10))) || (!validate.isNumber(parseInt(width, 10)))) {
+        return this.setState({error: 'Please enter a valid input (hint: must be a number)', clicked: false});
+      } return this.setState({error: '', clicked: true})
+    }
+    
+    const {error, clicked, width} = this.state;
+    if (error === '' && clicked) {
+      const row = [];
+      for (let i = 1; i <= width; i++) {
+        row.push(i)
+      }
+      return (
+        <div className="App">
+          {row.map(function(id, index) {
+            return <Box key={index}>{id}</Box>
+          })}
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Form onInputChange={onInputChange} onButtonClick={onButtonClick} error={error}/>
       </div>
     );
   }
---
+++ src/components/form.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+const Form = ({onInputChange, onButtonClick, error}) => (
+  <form>
+    <label className='error'>{error}</label>
+    <div>
+      <label htmlFor='height'>No of Heigth: </label>
+      <input onChange={onInputChange} name='height' id='height' type='text' required />
+    </div>
+    <div>
+      <label htmlFor='width'>No of Width: </label>
+      <input onChange={onInputChange} name='width' id='width' type='text' required />
+    </div>
+    <div>
+      <button onClick={onButtonClick}>Continue</button>
+    </div>
+  </form>
+)
+
+export default Form;
